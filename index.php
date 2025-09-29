44473526eae16424d8e06dc421461f8538aa2e32
hansjuwi <hans.juwiantho@petra.ac.id>
2025-09-29 09:41:56 +0700

    file awal php intro

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include "head.php"; ?>
+<body>
+    <p> Halo </p>
+    <div id="text"> ini text </div>
+<h1>Hello from Dr. Chuck's HTML Page</h1>
+<p>
+<?php
+    for($i=0;$i<=10;$i++)
+    {
+        for($j=0;$j<=10;$j++)
+        {
+            echo "*";
+        }
+        echo "</br>";
+    }
+?>
+</p>
+<p>Yes  <?php echo "Halo"; ?>  another paragraph.</p>
+
+</body>
+<script>
+    x = 5;
+    y = 10;
+    document.getElementById("text").innerHTML = "ini tulisan";
+
+</script>
+
+
+</html>
